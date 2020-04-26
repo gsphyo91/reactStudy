10c303b1a0f944c795070f36d133dbc1b9ce9feb
Gyungsoo Phyo <gsphyo91@gmail.com>
2020-04-26 17:50:52 +0900

    heading style

--- Inventory/react-blog-1/src/components/blog/Editor.jsx
+++ Inventory/react-blog-1/src/components/blog/Editor.jsx
@@ -1,15 +1,25 @@
-import React, { useState, Fragment, useRef, createRef } from 'react';
+import React, { useState, Fragment, useRef, createRef } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 const initial_style = `
-  font-size: inherit;
-`
+  font-size: 14px;
+  height: 30px;
+`;
 
 const heading1_style = `
-  font-size: 24px;
-  font-weight: bold;
-  height: 43px;
+  font-size: 1.875em;
+  font-weight: 600;
+`;
+
+const heading2_style = `
+  font-size: 1.5em;
+  font-weight: 600;
 `
+
+const heading3_style = `
+  font-size: 1.25em;
+  font-weight: 600;
+`;
 
 const Editor = () => {
   const inputFocus = useRef([createRef()]);
@@ -17,28 +27,42 @@ const Editor = () => {
     {
       type: "",
       data: "",
-    }
+    },
   ]);
 
   const handleInputChange = (index, event) => {
     const values = [...textFields];
     values[index].data = event.target.value;
-    if (event.target.value === '# ') {
+    if (event.target.value === "# ") {
       inputFocus.current[index].current.style = heading1_style;
-      values[index].type = 'h1';
-      values[index].data = '';
+      values[index].type = "h1";
+      values[index].data = "";
       setTextFields(values);
     }
-    setTextFields(values);
-  }
+    else if (event.target.value === "## "){
+      inputFocus.current[index].current.style = heading2_style;
+      values[index].type = "h2";
+      values[index].data = "";
+      setTextFields(values);
+    } 
+    else if (event.target.value === "### "){
+      inputFocus.current[index].current.style = heading3_style;
+      values[index].type = "h2";
+      values[index].data = "";
+      setTextFields(values);
+    } 
+    else {
+      setTextFields(values);
+    }
+  };
 
   const handleAddFields = () => {
     const values = [...textFields];
     // const focuses = [...inputFocus.current];
-    values.push({ type: '', data: "" });
+    values.push({ type: "", data: "" });
     inputFocus.current.push(createRef());
     setTextFields(values);
-  }
+  };
 
   const handleRemoveFields = (index, event) => {
     const values = [...textFields];
@@ -48,27 +72,29 @@ const Editor = () => {
         inputFocus.current.splice(index, 1);
         inputFocus.current[index - 1].current.focus();
         setTextFields(values);
-      }
-      else{
+      } else {
         handleLine(index, event);
       }
-    }
-    else{
-      if(values[index].data.length === 0){
+    } else {
+      if (values[index].data.length === 0) {
         inputFocus.current[index].current.style = initial_style;
-      }
-      else{
-        handleLine(index, event);
+      } else {
+        // handleLine(index, event);
       }
     }
-  }
+  };
 
   const handleLine = (index, event) => {
-    const lines = textFields[index].data.split(/\r*\n/);
-    console.log(lines.length);
-    console.log(textFields[index].data);
-    inputFocus.current[index].current.style = `height: ${28 + (22 * (lines.length))}px`;
-  }
+    const lines = event.target.value;
+    console.log(`lines : ${lines}`);
+    console.log(lines.split("\n"));
+    // const lines = textFields[index].data.split(/\r*\n/);
+    // console.log(lines);
+    // console.log(textFields[index].data);
+    // inputFocus.current[index].current.style = `height: ${
+    //   28 + 22 * lines.length
+    // }px`;
+  };
 
   return (
     <div className="container-content">
@@ -77,31 +103,31 @@ const Editor = () => {
       <TextareaAutosize />
       {textFields.map((textField, index) => (
         <Fragment key={`${textField}~${index}`}>
-          <textarea
+          <TextareaAutosize
             className="input"
             placeholder="Input here"
             name={`input${index}`}
             value={textField.data}
-            onChange={event => handleInputChange(index, event)}
-            onKeyDown={event => {
-              if (!event.shiftKey && event.key === 'Enter') {
+            onChange={(event) => handleInputChange(index, event)}
+            onKeyDown={(event) => {
+              if (!event.shiftKey && event.key === "Enter") {
                 event.preventDefault();
                 return handleAddFields();
-              }
-              else if(event.shiftKey && event.key === 'Enter'){
-                return handleLine(index, event);
-              }
-              else if (event.key === 'Backspace') {
+              // } else if (event.shiftKey && event.key === "Enter") {
+              //   return handleLine(index, event);
+              } else if (event.key === "Backspace") {
                 return handleRemoveFields(index, event);
+              }else{
+                console.log(event.key);
               }
             }}
-            ref={inputFocus.current[index]}
+            inputRef={inputFocus.current[index]}
             autoFocus
           />
         </Fragment>
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Editor;
