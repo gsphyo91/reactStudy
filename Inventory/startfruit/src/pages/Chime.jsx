4c89671ee801f747ab0d44e99e3ea43a39c03ec7
Gyungsoo Phyo <gsphyo@gmail.com>
2020-04-29 16:02:11 +0900

    video

--- Inventory/startfruit/src/pages/Chime.jsx
+++ Inventory/startfruit/src/pages/Chime.jsx
@@ -4,7 +4,8 @@ import {
   DefaultDeviceController,
   DefaultMeetingSession,
   LogLevel,
-  MeetingSessionConfiguration
+  MeetingSessionConfiguration,
+  DefaultModality
 } from 'amazon-chime-sdk-js';
 import { requestAPI } from "../components/api";
 
@@ -16,6 +17,7 @@ const Chime = () => {
   const [audioInputDeviceInfo, setAudioInputDeviceInfo] = useState({});
   const [audioOutputDeviceInfo, setAudioOutputDeviceInfo] = useState({});
   const [videoInputDeviceInfo, setVideoInputDeviceInfo] = useState({});
+  const [sessionStatus, setSessionStatus] = useState(false);
   const [cameraStatus, setCameraStatus] = useState(false);
   const [audioStatus, setAudioStatus] = useState(false);
   const [audioMuted, setAudioMuted] = useState(false);
@@ -41,18 +43,40 @@ const Chime = () => {
   const configureSession = async () => {
     const configuration = new MeetingSessionConfiguration(meetingResponse, attendeeResponse);
     console.log(configuration);
-    // setConfiguration(meetingConfiguration);
+
     meetingSession = new DefaultMeetingSession(configuration, logger, deviceController);
     console.log(meetingSession);
+
     const audioInputDevices = await meetingSession.audioVideo.listAudioInputDevices();
     console.log(audioInputDevices);
     const audioOutputDevices = await meetingSession.audioVideo.listAudioOutputDevices();
     console.log(audioOutputDevices);
     const videoInputDevices = await meetingSession.audioVideo.listVideoInputDevices();
     console.log(videoInputDevices);
+
     setAudioInputDeviceInfo(audioInputDevices[0]);
     setAudioOutputDeviceInfo(audioOutputDevices[0]);
     setVideoInputDeviceInfo(videoInputDevices[0]);
+
+    
+  }
+
+  const onToggleSession = () => {
+    if(sessionStatus){
+      try{
+        meetingSession.audioVideo.stop();
+        setSessionStatus(false);
+      } catch(err){
+        setSessionStatus(true);
+      }
+    }else{
+      try{
+        meetingSession.audioVideo.start();
+        setSessionStatus(true);
+      } catch(err){
+        setSessionStatus(false);
+      }
+    }
   }
 
   const onToggleAudio = async () => {
@@ -60,7 +84,7 @@ const Chime = () => {
     console.log(audioOutputDeviceInfo);
 
     if (audioStatus) {
-      try{
+      try {
         await meetingSession.audioVideo.chooseAudioInputDevice(null);
         await meetingSession.audioVideo.chooseAudioOutputDevice(null);
         stopAudioSession();
@@ -73,6 +97,13 @@ const Chime = () => {
         await meetingSession.audioVideo.chooseAudioInputDevice(audioInputDeviceInfo.deviceId);
         await meetingSession.audioVideo.chooseAudioOutputDevice(audioOutputDeviceInfo.deviceId);
         startAudioSession();
+        const muted = meetingSession.audioVideo.realtimeIsLocalAudioMuted();
+        console.log(`Audio isMuted? ${muted}`);
+        if (muted) {
+          setAudioMuted(true);
+        } else {
+          setAudioMuted(false);
+        }
         setAudioStatus(true);
       } catch (err) {
         console.log(`err!!! ${err}`);
@@ -97,24 +128,51 @@ const Chime = () => {
       }
     };
     meetingSession.audioVideo.addObserver(observer);
-    meetingSession.audioVideo.start();
+
+    // [시작] 오디오 음량 체크
+    const presentAttendeeId = meetingSession.configuration.credentials.attendeeId;
+    meetingSession.audioVideo.realtimeSubscribeToVolumeIndicator(
+      presentAttendeeId,
+      (attendeeId, volume, muted, signalStrength) => {
+        const baseAttendeeId = new DefaultModality(attendeeId).base();
+        if(baseAttendeeId !== attendeeId){
+          console.log(`The volume of ${baseAttendeeId}'s content changes`);
+        }
+        console.log(`${attendeeId}'s volume data: `, {
+          volume,
+          muted,
+          signalStrength
+        });
+      }
+    )
+    // [끝] 오디오 음량 체크
   }
 
-  const stopAudioSession = () => {
-    meetingSession.audioVideo.stop();
+  const stopAudioSession = () => {}
+
+  const onToggleAudioMute = () => {
+    if (audioMuted) {
+      meetingSession.audioVideo.realtimeUnmuteLocalAudio();
+      setAudioMuted(false);
+      console.log("Audio Unmute");
+    } else {
+      meetingSession.audioVideo.realtimeMuteLocalAudio();
+      setAudioMuted(true);
+      console.log("Audio Mute");
+    }
   }
 
   const onToggleCamera = async () => {
     if (cameraStatus) {
       await meetingSession.audioVideo.chooseVideoInputDevice(null);
+      stopVideoSession();
       setCameraStatus(false);
       videoRef.current.style = 'width: 100px; height: 100px; background-color: blue;';
-      // stopVideoSession();
     } else {
       await meetingSession.audioVideo.chooseVideoInputDevice(videoInputDeviceInfo.deviceId);
+      videoRef.current.style = 'width: 680px; height: 480px;';
+      startVideoSession();
       setCameraStatus(true);
-      videoRef.current.style = 'width: 100px; height: 100px; background-color: red;';
-      // startVideoSession();
     }
   }
 
@@ -131,7 +189,7 @@ const Chime = () => {
     }
     meetingSession.audioVideo.addObserver(observer);
     meetingSession.audioVideo.startLocalVideoTile();
-    meetingSession.audioVideo.start();
+    // meetingSession.audioVideo.start();
   }
 
   const stopVideoSession = () => {
@@ -155,7 +213,7 @@ const Chime = () => {
     };
     meetingSession.audioVideo.addObserver(observer);
     meetingSession.audioVideo.stopLocalVideoTile();
-    // meetingSession.audioVideo.removeLocalVideoTile();
+    meetingSession.audioVideo.removeLocalVideoTile();
   }
 
 
@@ -164,20 +222,20 @@ const Chime = () => {
       <div>
         <p>Chime</p>
         <button onClick={getMeetingInfo}>1. Create Meeting</button>
-        {/* {console.log(meetingResponse.Meeting)} */}
-        {/* {console.log(attendeeResponse.Attendee)} */}
         <p>Meeting ID : {meetingResponse.Meeting ? meetingResponse.Meeting.MeetingId : null}</p>
         <p>Attendee ID : {attendeeResponse.Attendee ? attendeeResponse.Attendee.AttendeeId : null}</p>
-        <button onClick={configureSession}>2. Session</button>
+        <button onClick={configureSession}>2. Set a Session</button>
+        <button onClick={onToggleSession}>Session {sessionStatus ? 'Stop' : 'Start'}</button>
         <button onClick={onToggleAudio}>3. Audio {audioStatus ? 'Off' : 'On'}</button>
-        {/* <button onClick={}>{audioMuted ? "UnMute" : "Mute"}</button> */}
-        {/* <button onClick={onToggleCamera}>Camera {cameraStatus ? 'Off' : 'On'}</button> */}
-
+        <button onClick={onToggleAudioMute}>4. {audioMuted ? "UnMute" : "Mute"}</button>
+      </div>
+      <div>
+        <button onClick={onToggleCamera}>5. Camera {cameraStatus ? 'Off' : 'On'}</button>
       </div>
       {/* {cameraStatus ? <video className="video-test" ref={videoRef}></video>: null} */}
       <div>
-        {/* <video className="video-test" ref={videoRef}></video> */}
-        <audio id="audio-test" ref={audioRef}></audio>
+        <video className="video-test" ref={videoRef}></video>
+        <audio ref={audioRef}></audio>
       </div>
     </div>
   )
